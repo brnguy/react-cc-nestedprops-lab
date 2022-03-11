7e5d5e9862978a812d4bf95e5d9c446cfb2a9d87
brnguy <nguyen.bryan15@gmail.com>
2022-03-11 15:09:39 -0800

    Refactored to functional components

--- src/Score.js
+++ src/Score.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-
-class Score extends Component {
-    render() { 
-        return (
-            <>
-                <p>Date: {this.props.date}</p>
-                <p>Score: {this.props.score}</p>
-            </>
-        );
-    }
+function Score({content}) {
+    return ( 
+        <>
+            <p>Date:{content.date}</p>
+            <p>Score: {content.score}</p>
+        </>
+     );
 }
- 
+
 export default Score;
+
+{/* // import React, { Component } from 'react';
+
+// class Score extends Component { */}
+{/* //     render() {  */}
+{/* //         return (
+//             <>
+//                 <p>Date: {this.props.date}</p>
+//                 <p>Score: {this.props.score}</p>
+//             </>
+//         );
+//     }
+// }
+ 
+// export default Score; */}
--- src/Student.js
+++ src/Student.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import Score from './Score'
 
-class Student extends Component {
-    render() { 
-        const scoreComponents = this.props.scores.map((score, index) => {
-            return(
-                <Score
-                    key={`Score-${index}`}
-                    date={score.date}
-                    score={score.score}
-                />
-            )
-        })
-        return (
-            <>
-            <h1>Name:{this.props.name}</h1>
-            <p>Bio:{this.props.bio}</p>
+
+function Student({ name, bio, scores }) {
+    const scoreComponents = scores.map((score, index) => {
+        return(
+            <Score
+                key={`Score-${index}`}
+                content={score}
+            />
+        )
+    })
+    return(
+        <>
+            <h1>Name:{name}</h1>
+            <p>Bio:{bio}</p>
             {scoreComponents}
-            </>
-        );
-    }
+        </>
+    )
 }
- 
+
 export default Student;
+
+// class Student extends Component {
+//     render() { 
+//         const scoreComponents = this.props.scores.map((score, index) => {
+//             return(
+//                 <Score
+//                     key={`Score-${index}`}
+//                     date={score.date}
+//                     score={score.score}
+//                 />
+//             )
+//         })
+//         return (
+//             <>
+//             <h1>Name:{this.props.name}</h1>
+//             <p>Bio:{this.props.bio}</p>
+//             {scoreComponents}
+//             </>
+//         );
+//     }
+// }
+ 
+// export default Student;
